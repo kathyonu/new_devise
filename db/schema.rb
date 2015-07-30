c98437de2de57f87db17fc2f578d9b4ce2f3987a
Nikolay Nikolov <nikolay12@gmail.com>
2015-07-31 00:04:52 +0100

    Added NOT NULL constraints to Subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726120054) do
+ActiveRecord::Schema.define(version: 20150730205354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 20150726120054) do
   add_index "plans", ["stripe_id"], name: "index_plans_on_stripe_id", unique: true, using: :btree
 
   create_table "subscriptions", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "plan_id"
+    t.integer  "user_id",    null: false
+    t.integer  "plan_id",    null: false
     t.string   "stripe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
