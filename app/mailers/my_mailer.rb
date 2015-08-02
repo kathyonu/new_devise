427de90f1055895ded68f1f5e3c587334f64730b
Nikolay Nikolov <nikolay12@gmail.com>
2015-08-02 14:26:46 +0100

    Adding the password and the plan name to the welcome email

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -2,7 +2,9 @@ class MyMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
 
-  def welcome(record, opts={})
+  def welcome(record, token, opts={})
+    @plan = opts[:plan]
+    @token = token
     devise_mail(record, :welcome, opts)
   end
 end
