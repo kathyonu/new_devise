bb59502d9ad6c6637964dce1203d57570354662c
Nikolay Nikolov <nikolay12@gmail.com>
2015-09-01 09:49:06 +0100

    Legacy: validations seem to have benn added; the uniqueness of emails might need a custom err message, e.g. *You already have an account. Forgot your password?*

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_confirmation_of :email, :on => :create
+  validates_presence_of :email, :email_confirmation
+  validates_format_of :email, :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, :on => :create, :message => "is invalid"
+  validates_uniqueness_of :email
   has_paper_trail
 end
